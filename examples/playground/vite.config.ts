611337427982f6bd6badac65b97ea51c72af5792
mouse_484 <38714187+mouse484@users.noreply.github.com>
2024-01-03 05:54:27 +0000

    add biome

--- examples/playground/vite.config.ts
+++ examples/playground/vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig, normalizePath } from 'vite';
+import { defineConfig } from "vite";
 
+// biome-ignore lint/nursery/noDefaultExport: <explanation>
 export default defineConfig({
-  resolve: {
-    alias: {
-      chibivue: `${process.cwd()}/../../packages/chibivue`,
-    },
-  },
+	resolve: {
+		alias: {
+			chibivue: `${process.cwd()}/../../packages/chibivue`,
+		},
+	},
 });
