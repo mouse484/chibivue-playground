e84883d13c2e2b34fb037d22377882c6102b4e78
mouse_484 <38714187+mouse484@users.noreply.github.com>
2024-06-21 06:53:48 +0000

    add lint

--- examples/playground/vite.config.js
+++ examples/playground/vite.config.js
@@ -1,12 +1,12 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { defineConfig } from 'vite';
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
 
-const dirname = path.dirname(fileURLToPath(new URL(import.meta.url)));
+const dirname = path.dirname(fileURLToPath(new URL(import.meta.url)))
 export default defineConfig({
   resolve: {
     alias: {
       chibivue: path.resolve(dirname, '../../packages'),
     },
   },
-});
+})
